1e24642bd5636ef74c425ea6a9eb57862612b634
Gangadhar C <gangadharganga90@gmail.com>
2023-04-21 09:35:13 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_Recharge_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
